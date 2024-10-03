4884484066a1b7240f4e775d1a1bc566803603e9
Tealwine <ngomuimac@gmail.com>
2024-10-03 10:38:36 +0700

    update multiplication funtion

--- GIUAKY_NHOM1/GIUAKY_NHOM1/Siu.cs
+++ GIUAKY_NHOM1/GIUAKY_NHOM1/Siu.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace GIUAKY_NHOM1
 {
@@ -31,5 +32,15 @@ namespace GIUAKY_NHOM1
         {
 
         }
-    }
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			double num1 = double.Parse(txt1.Text);
+			double num2 = double.Parse(txt2.Text);
+
+			// Thực hiện phép nhân
+			double result = num1 * num2;
+            txtketqua.Text = result.ToString();
+		}
+	}
 }
